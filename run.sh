967c07102200994ded36b52b28286a2d98553aa3
bxhoa <hoabuixuan0410@gmail.com>
2025-11-23 22:30:01 +0700

    update run,sh

--- run.sh
+++ run.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Optional: activate virtual environment if you use one
-# source .venv/bin/activate
-
-# Move to project root (directory of this script)
-cd "$(dirname "$0")"
-
-# Train Stage 2 segmentation on DFC2020 using SegNet
-# Hyperparameters (batch_size, num_epochs, lrs, etc.) are taken from config.py (STAGE2)
+#cd "$(dirname "$0")"
+echo  "=================== Starting Training ================="
 
 python training_decoder.py \
   --model segnet \
   --backbone resnet50 \
-  --encoder_checkpoint checkpoints/stage1_encoder.pth \
   --num_classes 19
+
+echo  "=================== END Training ================="
