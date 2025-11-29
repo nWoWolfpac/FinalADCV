22008fcd6dc4f274b26dba066221052ebeb17423
nWoWolfpac <minhduc5clhp@gmail.com>
2025-11-29 20:05:17 +0700

    Update segnet.py

--- src/models/segnet.py
+++ src/models/segnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from src.models.encoder import EncoderClassifier
+from src.models.encoder import EncoderBackbone as EncoderClassifier
 
 
 class UpBlock(nn.Module):
